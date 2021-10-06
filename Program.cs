1cb4686ec45952ac1694a8983b65e1306131b565
Raghav <raghavwandre1234@gmail.com>
2021-10-06 16:27:52 +0530

    Program File for UC2

--- Program.cs
+++ Program.cs
@@ -7,19 +7,35 @@ namespace Day15_Hash_TableAss
     {
         static void Main(string[] args)
         {
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "To");
-            hash.Add("5", "be");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(19);
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoids");
+            hash.Add("4", "Because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "deliberately");
+            hash.Add("15", "into");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situation");
 
-            string hash1 = hash.Get("4");
 
-
+            string hash1 = hash.Get("11");
             Console.WriteLine(hash1);
 
+
+            UC2 hash2 = new UC2();
+            hash2.FreqLarge();
         }
     }
 }
+
